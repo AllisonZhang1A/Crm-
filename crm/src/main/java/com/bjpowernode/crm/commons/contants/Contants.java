ab40e60dd927c63207a6d4ef2f8c902278b1f138
zhangyian <canlucingning@gmail.com>
2022-08-27 04:05:38 +0800

    实现市场活动备注

--- crm/src/main/java/com/bjpowernode/crm/commons/contants/Contants.java
+++ crm/src/main/java/com/bjpowernode/crm/commons/contants/Contants.java
@@ -7,4 +7,9 @@ public class Contants {
 
     //保存当前用户的key
     public static final String SESSION_USER="sessionUser";
+
+    //备注的修改标记:0表示修改过 1表示修改过
+    public static final String REMARK_EDIT_FLAG_NO_EDIT="0";
+    public static final String REMARK_EDIT_FLAG_YES_EDIT="1";
+
 }
--- crm/src/main/java/com/bjpowernode/crm/workbench/service/ActivityRemarkService.java
+++ crm/src/main/java/com/bjpowernode/crm/workbench/service/ActivityRemarkService.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface ActivityRemarkService {
     List<ActivityRemark> queryActivityRemarkForDetailByActivityId(String activityId);
 
+    int saveCreateActivityRemark(ActivityRemark remark);
 }
